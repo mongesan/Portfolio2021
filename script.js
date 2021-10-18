036e06188bc79b2d403264821942ded98c74686d
sakamoto <syun1.mongesan@gmail.com>
2021-10-18 22:50:03 +0900

    today's work

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    hsize = $(window).height();
-    $("header").css("height", hsize + "px");
-});
+// $(document).ready(function () {
+//     hsize = $(window).height();
+//     $("header").css("height", hsize + "px");
+// });
 
-$(window).resize(function () {
-    hsize = $(window).height();
-    $("header").css("height", hsize + "px");
-});
+// $(window).resize(function () {
+//     hsize = $(window).height();
+//     $("header").css("height", hsize + "px");
+// });
