5561426a658e73287465d477dc02b99889d539f6
sakamoto <syun1.mongesan@gmail.com>
2021-10-18 22:07:19 +0900

    Only structure

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    hsize = $(window).height();
+    $("header").css("height", hsize + "px");
+});
+
+$(window).resize(function () {
+    hsize = $(window).height();
+    $("header").css("height", hsize + "px");
+});
